a6004460d274e37947551b23b5261073948742da
Alejandro Munyoz Belmar <almube@floridauniversitaria.es>
2016-10-19 12:32:02 +0200

    prueba programacion

---
+++ Animal.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class animal
+{
+
+ public $animal="perro";
+ public $patas=4;
+
+public function getpatas(){
+  return $this->$patas;
+}
+public function getanimal(){
+  return $this->$animal;
+
+}
+public function setanimal($animal){
+  
+}
+public function setpatas($patas){
+
+}
+}
+?>
